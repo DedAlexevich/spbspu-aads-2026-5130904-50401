75384091ad5f5ddb9233b806860f7ece865dfe61
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-03-09 12:11:18 +0300

    Declare a struct of iterator

--- kuznetsov.petr/S1/list.hpp
+++ kuznetsov.petr/S1/list.hpp
@@ -11,7 +11,24 @@ namespace kuznetsov {
       Node< T >* prev;
     };
   }
-  
+
+  template< class T >
+  class List;
+
+  template< class T >
+  class LIter {
+    friend class List< T >;
+    Node< T >* curr;
+  public:
+    LIter& operator++();
+    LIter& operator++(int);
+    LIter& operator--();
+    LIter& operator--(int);
+    T& operator*();
+    bool operator==(const List& __x, const List& __y);
+    bool operator!=(const List& __x, const List& __y);
+  };
+
 
 }
 
