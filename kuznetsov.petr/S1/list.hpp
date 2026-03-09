195d6461f2f0457ef4e4a15407cab1b2ea8f1c28
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-03-09 13:15:11 +0300

    Declaration of the Leaf class. Implementation of the constructor, destructor, and cleanup methods

--- kuznetsov.petr/S1/list.hpp
+++ kuznetsov.petr/S1/list.hpp
@@ -6,9 +6,9 @@ namespace kuznetsov {
   namespace {
     template< class T >
     struct Node {
-      T val;
-      Node< T >* next;
-      Node< T >* prev;
+      T val_;
+      Node< T >* next_;
+      Node< T >* prev_;
     };
   }
 
@@ -17,12 +17,10 @@ namespace kuznetsov {
 
   template< class T >
   class LCIter {
-    friend class List< T >;
-    Node< T >* curr;
   public:
     LCIter& operator++()
     {
-      this->curr = this->curr->next;
+      this->curr_ = this->curr_->next_;
       return this;
     }
     LCIter& operator++(int)
@@ -33,7 +31,7 @@ namespace kuznetsov {
     }
     LCIter& operator--()
     {
-      this->curr = this->curr->prev;
+      this->curr_ = this->curr_->prev_;
       return this;
     }
     LCIter& operator--(int)
@@ -44,25 +42,72 @@ namespace kuznetsov {
     }
     const T& operator*()
     {
-      return curr->val;
+      return curr_->val;
     }
     bool operator==(const LCIter& y)
     {
-      return this->curr == y.curr;
+      return this->curr_ == y.curr_;
     }
 
     bool operator!=(const LCIter& y)
     {
       return !(this == y);
     }
+  private:
+    friend class List< T >;
+    Node< T >* curr_;
   };
 
   template< class T >
   class LIter : LCIter< T > {
     T& operator*()
     {
-      return curr->val;
+      return curr_->val;
     }
+  };
+
+  template< class T >
+  class List {
+  public:
+    List(): head_(nullptr), size_(0) {}
+    ~List()
+    {
+      clear();
+    }
+    LIter< T > insert(LCIter< T > it, T& val);
+    T& front();
+    T& back();
+    void popFront();
+    void popBack();
+    void pop(LIter< T > it);
+    LIter< T > begin();
+    LIter< T > end();
+    LCIter< T > cbegin();
+    LCIter< T > cend();
+    void clear()
+    {
+      Node< T >* t = nullptr;
+      for (size_t i = 0; i < size_; ++i) {
+        t = head_->next_;
+        delete head_;
+        head_ = t;
+      }
+      size_ = 0;
+      head_ = nullptr;
+    }
+    LIter< T > erase(LCIter< T > it)
+    {
+      LIter< T > t(it.curr_->next_);
+      it.curr_->prev_->next_ = it.curr_->next_;
+      it.curr_->next_->prev_ = it.curr_->prev_;
+      delete it.curr_;
+      return t;
+    }
+
+  private:
+    Node< T >* head_;
+    size_t size_;
+
   };
 
 }
