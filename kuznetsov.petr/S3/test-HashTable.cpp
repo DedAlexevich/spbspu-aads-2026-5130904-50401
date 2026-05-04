f358205a083e44d81830150f9ee6040918d149ae
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-05-04 17:57:07 +0300

    Add test to check work of my HashTable while i am developing hashtable

---
+++ kuznetsov.petr/S3/test-HashTable.cpp
@@ -0,0 +1,18 @@
+#include <boost/test/unit_test.hpp>
+#include <cstddef>
+#include <functional>
+#include "HashTable.hpp"
+
+bool comp(long a, long b)
+{
+  return a == b;
+}
+
+BOOST_AUTO_TEST_CASE(createDefaultHashTable)
+{
+  HashTable< long, int, std::hash, comp> map;
+  BOOST_TEST(map.getSize() == 0);  
+  BOOST_TEST(map.getCapacity() == 10);
+}
+
+
