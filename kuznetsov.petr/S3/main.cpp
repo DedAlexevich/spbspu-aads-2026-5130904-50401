ff3ef182f311f80d940b2e91bf558c355b623cc0
Kuznetsov Petr <kuznecovpetr502@yandex.ru>
2026-05-08 02:17:45 +0300

    Declare struct Edge and hash functor

--- kuznetsov.petr/S3/main.cpp
+++ kuznetsov.petr/S3/main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
+#include <boost/hash2/siphash.hpp>
+#include <fstream>
+#include "../common/includes/top-it-vector.hpp"
+#include "./HashTable.hpp"
 
-int main()
-{}
+namespace kuznetsov {
+
+  struct Edge {
+    std::string from_;
+    std::string to_;
+  };
+
+  struct Hasher {
+    size_t operator()(Edge e);
+  };
+
+
+}
+
+int main(int argc, char** argv)
+{
+  if (argc < 2) {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+  std::ifstream input(argv[1]);
+  if (!input) {
+    std::cerr << "Couldn't open file\n";
+    return 1;
+  }
+
+  std::string name;
+  size_t count = 0;
+  while (input >> name >> count) {
+    std::cout << name << '\n';
+    std::cout << count << '\n';
+  }
+
+}
 
