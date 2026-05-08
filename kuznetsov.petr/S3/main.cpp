5df4249ca707f0f55ae17637cd1ba98555730cf2
Kuznetsov Petr <kuznecovpetr502@yandex.ru>
2026-05-09 00:19:18 +0300

    Read graph from file

--- kuznetsov.petr/S3/main.cpp
+++ kuznetsov.petr/S3/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <boost/hash2/siphash.hpp>
+#include <boost/hash2/hash_append.hpp>
+#include <boost/describe/class.hpp>
 #include <fstream>
 #include "../common/includes/top-it-vector.hpp"
 #include "./HashTable.hpp"
@@ -11,14 +13,35 @@ namespace kuznetsov {
     std::string to_;
   };
 
+  BOOST_DESCRIBE_STRUCT(Edge, (), (from_, to_));
   struct Hasher {
-    size_t operator()(const Edge& e) const;
-    size_t operator()(const std::string&) const;
+    size_t operator()(const Edge& e) const
+    {
+      boost::hash2::siphash_64 hasher(seed);
+      boost::hash2::hash_append(hasher, {}, e);
+      return hasher.result();
+    }
+    size_t operator()(const std::string& s) const
+    {
+      boost::hash2::siphash_64 hasher(seed);
+      hasher.update(s.data(), s.size());
+      return hasher.result();
+    }
+  private:
+    size_t seed;
   };
-  
+
   struct KeyComparator {
-    bool operator()(const Edge& e) const;
-    bool operator()(const std::string&) const;
+    bool operator()(const Edge& e1, const Edge& e2) const
+    {
+      bool f = e1.from_ == e2.from_;
+      f = f && e1.to_ == e2.to_;
+      return f;
+    }
+    bool operator()(const std::string& s1, const std::string& s2) const
+    {
+      return s1 == s2;
+    }
   };
 
 }
@@ -44,14 +67,19 @@ int main(int argc, char** argv)
   while (input >> name >> count) {
     graph t(count);
     for (size_t i = 0; i < count; ++i) {
-      Edge e;
+      kuz::Edge e;
       size_t weight;
       input >> e.from_ >> e.to_ >> weight;
-      t.add(e, weight);
+      if (t.has(e)) {
+        t.at(e).pushBack(weight);
+      } else {
+        t.add(e, kuz::Vector< size_t >());
+        t.at(e).pushBack(weight);
+      }
     }
     graphs.add(name, t);
   }
-
+  std::cout << graphs.getSize() << '\n';
 
 }
 
