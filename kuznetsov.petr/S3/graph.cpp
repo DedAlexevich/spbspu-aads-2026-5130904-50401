39e2e6ef20fa0b947ab8cae27b968ec172df7ca0
Kuznetsov Petr <kuznecovpetr502@yandex.ru>
2026-05-10 14:16:24 +0300

    Allocated graph to separate struct

---
+++ kuznetsov.petr/S3/graph.cpp
@@ -0,0 +1,69 @@
+#include "./graph.hpp"
+#include <iostream>
+#include <stdexcept>
+#include "../common/includes/top-it-iters.hpp"
+
+kuznetsov::Graph::Graph():
+  table_(),
+  vertexes_()
+{}
+
+void kuznetsov::Graph::addVertexes(const std::string& name)
+{
+  if (vertexes_.contain(name)) {
+    throw std::logic_error("Such vertex already exist");
+  }
+  vertexes_.pushBack(name);
+}
+
+void kuznetsov::Graph::removeVertexes(const std::string& name)
+{
+  if (!vertexes_.contain(name)) {
+    throw std::logic_error("Such vertex not exist");
+  }
+
+  for (auto it = vertexes_.cbegin(); it != vertexes_.cend(); ++it) {
+    if (*it == name) {
+      vertexes_.erase(it);
+      break;
+    }
+  }
+
+  for (auto it = table_.begin(); it != table_.end(); ++it) {
+    if ((*it).first.first == name || (*it).first.second == name) {
+      table_.remove((*it).first);
+    }
+  }
+
+}
+
+void kuznetsov::Graph::addEdge(const std::string& v1, const std::string& v2, size_t w)
+{
+  auto k = std::make_pair(v1, v2);
+  if (table_.has(k)) {
+    table_.at(k).pushBack(w);
+  } else {
+    table_.add(k, Vector< size_t >());
+    table_.at(k).pushBack(w);
+  }
+}
+
+void kuznetsov::Graph::removeEdge(const std::string& v1, const std::string& v2, size_t w)
+{
+  auto k = std::make_pair(v1, v2);
+  if (table_.has(k)) {
+    auto slot = table_.at(k);
+    if (slot.getSize() > 1) {
+      for (auto it = slot.cbegin(); it != slot.cend(); ++it) {
+        if ((*it) == w) {
+          slot.erase(it);
+          return;
+        }
+      }
+    } else {
+      table_.remove(k);
+    }
+  }
+}
+
+
---
+++ kuznetsov.petr/S3/graph.hpp
@@ -0,0 +1,45 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+#include <utility>
+#include <string>
+#include <cstddef>
+#include "../common/includes/top-it-vector.hpp"
+#include "./HashTable.hpp"
+#include "Hasher.hpp"
+namespace kuznetsov {
+  using key = std::pair< std::string, std::string >;
+
+  struct KeyComparator {
+    bool operator()(const key& e1, const key& e2) const
+    {
+      bool f = e1.first == e2.first;
+      f = f && e1.second == e2.second;
+      return f;
+    }
+
+    bool operator()(const std::string& s1, const std::string& s2) const
+    {
+      return s1 == s2;
+    }
+  };
+
+
+  struct Graph {
+
+    Graph();
+
+    void addVertexes(const std::string& name);
+    void removeVertexes(const std::string& name);
+    void addEdge(const std::string& v1, const std::string& v2, size_t w);
+    void removeEdge(const std::string& v1, const std::string& v2, size_t w);
+
+  private:
+    HashTable< key, Vector< size_t >, SipHasher< key >, KeyComparator > table_;
+    Vector< std::string > vertexes_;
+  };
+}
+
+
+
+#endif
+
