0f0adbf113393d5286a054f6c8ccac6dfb2fd640
Kuznetsov Petr <kuznecovpetr502@yandex.ru>
2026-05-23 15:34:47 +0300

    Move definition from declaration

--- kuznetsov.petr/common/queue.hpp
+++ kuznetsov.petr/common/queue.hpp
@@ -5,54 +5,63 @@ namespace kuznetsov {
   template< class T >
   class Queue {
   public:
-    T& front()
-    {
-      return list_.front();
-    }
-    const T& front() const
-    {
-      return list_.front();
-    }
+    T& front();
+    const T& front() const;
 
-    T& back()
-    {
-      return list_.back();
-    }
+    void push(const T& val);
 
-    const T& back() const
-    {
-      return list_.back();
-    }
+    void pop();
 
-    void push(const T& val)
-    {
-      list_.insert(list_.end(), val);
-    }
+    bool empty() const;
 
-    void pop()
-    {
-      list_.popFront();
-    }
+    size_t size() const;
 
-    bool empty() const
-    {
-      return list_.empty();
-    }
-
-    size_t size() const
-    {
-      return list_.size();
-    }
-
-    void clear() noexcept
-    {
-      list_.clear();
-    }
-
+    void clear() noexcept;
   private:
     List< T > list_;
   };
 }
 
+template< class T >
+T& kuznetsov::Queue< T >::front()
+{
+  return list_.front();
+}
+
+template< class T >
+const T& kuznetsov::Queue< T >::front() const
+{
+  return list_.front();
+}
+
+template< class T >
+void kuznetsov::Queue< T >::push(const T& val)
+{
+  list_.insert(list_.end(), val);
+}
+
+template< class T >
+void kuznetsov::Queue< T >::pop()
+{
+  list_.popFront();
+}
+
+template< class T >
+bool kuznetsov::Queue< T >::empty() const
+{
+  return list_.empty();
+}
+
+template< class T >
+size_t kuznetsov::Queue< T >::size() const
+{
+  return list_.size();
+}
+
+template< class T >
+void kuznetsov::Queue< T >::clear() noexcept
+{
+  list_.clear();
+}
 #endif
 
