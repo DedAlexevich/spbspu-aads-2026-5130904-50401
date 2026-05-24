818c3a8d6a84c0801ac151e8b32c62a0af5f39a5
Kuznetsov Petr <kuznecovpetr502@yandex.ru>
2026-05-24 14:27:14 +0300

    Fix rbegin

--- kuznetsov.petr/common/list.hpp
+++ kuznetsov.petr/common/list.hpp
@@ -156,7 +156,7 @@ namespace kuznetsov {
 
     LRCIter< T > rcend() const;
 
-    void clear();
+    void clear() noexcept;
 
     LIter< T > erase(LCIter< T > it);
 
@@ -610,6 +610,7 @@ kuznetsov::LCIter< T > kuznetsov::List< T >::cend() const
 template< class T >
 kuznetsov::LRIter< T > kuznetsov::List< T >::rbegin()
 {
+  assert(head_ != nullptr);
   return LRIter< T >(head_->prev_);
 }
 
@@ -622,6 +623,7 @@ kuznetsov::LRIter< T > kuznetsov::List< T >::rend()
 template< class T >
 kuznetsov::LRCIter< T > kuznetsov::List< T >::rcbegin() const
 {
+  assert(head_ != nullptr);
   return LRCIter< T >(head_->prev_);
 }
 
@@ -632,7 +634,7 @@ kuznetsov::LRCIter< T > kuznetsov::List< T >::rcend() const
 }
 
 template< class T >
-void kuznetsov::List< T >::clear()
+void kuznetsov::List< T >::clear() noexcept
 {
   if (!head_) {
     return;
