9108cc4745853283037d7c5762c0e7f65c19710b
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-06-09 19:07:11 +0300

    Implement RoadType::cost(double)

--- kuznetsov.petr/F0/model.cpp
+++ kuznetsov.petr/F0/model.cpp
@@ -1,9 +1,13 @@
 #include "model.hpp"
-
+#include <math.h>
 bool kuznetsov::detail::StrEqual::operator()(const std::string& a, const std::string& b) const
 {
   return a == b;
 }
 
+double kuznetsov::RoadType::cost(double x) const
+{
+  return k * std::pow(x, n) + b;
+}
 
 
