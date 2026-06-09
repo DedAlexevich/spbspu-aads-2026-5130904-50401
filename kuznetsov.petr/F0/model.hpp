33fd09e78956c75d4467a9027836f2534fdcdac3
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-06-09 18:50:35 +0300

    Declare map

--- kuznetsov.petr/F0/model.hpp
+++ kuznetsov.petr/F0/model.hpp
@@ -4,6 +4,7 @@
 #include <vector.hpp>
 #include <vectorIterators.hpp>
 #include "Hasher.hpp"
+#include "map.hpp"
 #include "unorderedMap.hpp"
 
 namespace kuznetsov {
@@ -39,6 +40,50 @@ namespace kuznetsov {
     std::string to;
     size_t importance;
   };
+
+  struct RouteStep {
+    std::string desc;
+    double cost;
+  };
+
+  struct Map {
+    StrHashMap< City > cities;
+    map< std::string, RoadType > transports;
+    Vector< Order > orders;
+
+    Vector< RouteStep > route;
+    double routeCost;
+    bool hasRoute;
+    size_t cursor;
+
+    Map();
+
+    bool hasCity(const std::string& name) const;
+    bool hasTransport(const std::string& type) const;
+    bool hasTerminal(const std::string& city, const std::string& type) const;
+    bool hasOrder(const std::string& id) const;
+    bool hasRoad(const std::string& type, const std::string& a, const std::string& b) const;
+
+    double terminalCost(const std::string& city, const std::string& type) const;
+
+    void addCity(const std::string& name);
+    void removeCity(const std::string& name);
+
+    void addTransport(const std::string& type);
+    void setRoadType(const std::string& type, double k, double b, double n);
+
+    void addTerminal(const std::string& city, const std::string& type, double cost);
+
+    void addRoad(const std::string& type, const std::string& a, const std::string& b, size_t dist);
+    void removeRoad(const std::string& type, const std::string& a, const std::string& b);
+
+    void addOrder(const Order& o);
+    void removeOrder(const std::string& id);
+
+    void clear();
+    void dropRoute();
+  };
+
 }
 
 #endif
