4eb31f69407c1481ed65a3012a2bee2566a5e272
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-06-13 20:04:21 +0300

    Implement add commands

--- kuznetsov.petr/F0/commands.cpp
+++ kuznetsov.petr/F0/commands.cpp
@@ -42,6 +42,34 @@ bool kuznetsov::detail::readOptional(std::istream& in, std::string& out)
   return static_cast< bool >(in >> out);
 }
 
+void kuznetsov::addTransport(std::ostream&, std::istream& in, MapsController& m)
+{
+  m.activeMap().addTransport(detail::reqStr(in));
+}
+
+void kuznetsov::roadType(std::ostream&, std::istream& in, MapsController& m)
+{
+  std::string name = detail::reqStr(in);
+  double k = detail::reqDouble(in);
+  double b = detail::reqDouble(in);
+  double n = detail::reqDouble(in);
+  m.activeMap().setRoadType(name, k, b, n);
+}
+
+void kuznetsov::addCity(std::ostream&, std::istream& in, MapsController& m)
+{
+  m.activeMap().addCity(detail::reqStr(in));
+}
+
+void kuznetsov::addTerminal(std::ostream&, std::istream& in, MapsController& m)
+{
+  std::string city = detail::reqStr(in);
+  std::string type = detail::reqStr(in);
+  double cost = detail::reqDouble(in);
+  m.activeMap().addTerminal(city, type, cost);
+}
+
+
 
 
 
--- kuznetsov.petr/F0/commands.hpp
+++ kuznetsov.petr/F0/commands.hpp
@@ -6,8 +6,8 @@
 #include <string>
 
 namespace kuznetsov {
-  using command = void (*)(std::ostream&, std::istream&, Manager&);
-  using commandConst = void (*)(std::ostream&, std::istream&, const Manager&);
+  using command = void (*)(std::ostream&, std::istream&, MapsController&);
+  using commandConst = void (*)(std::ostream&, std::istream&, const MapsController&);
 
   namespace detail {
     std::string reqStr(std::istream& in);
@@ -16,30 +16,30 @@ namespace kuznetsov {
     bool readOptional(std::istream& in, std::string& out);
   }
 
-  void addTransport(std::ostream&, std::istream&, Manager&);
-  void roadType(std::ostream&, std::istream&, Manager&);
-  void addCity(std::ostream&, std::istream&, Manager&);
-  void addTerminal(std::ostream&, std::istream&, Manager&);
-  void addRoad(std::ostream&, std::istream&, Manager&);
-  void removeCity(std::ostream&, std::istream&, Manager&);
-  void removeRoad(std::ostream&, std::istream&, Manager&);
-  void addOrder(std::ostream&, std::istream&, Manager&);
-  void removeOrder(std::ostream&, std::istream&, Manager&);
-  void optimizeCmd(std::ostream&, std::istream&, Manager&);
-  void nextStep(std::ostream&, std::istream&, Manager&);
-  void prevStep(std::ostream&, std::istream&, Manager&);
-  void loadCmd(std::ostream&, std::istream&, Manager&);
-  void newMapCmd(std::ostream&, std::istream&, Manager&);
-  void switchMapCmd(std::ostream&, std::istream&, Manager&);
-  void removeMapCmd(std::ostream&, std::istream&, Manager&);
+  void addTransport(std::ostream&, std::istream&, MapsController&);
+  void roadType(std::ostream&, std::istream&, MapsController&);
+  void addCity(std::ostream&, std::istream&, MapsController&);
+  void addTerminal(std::ostream&, std::istream&, MapsController&);
+  void addRoad(std::ostream&, std::istream&, MapsController&);
+  void removeCity(std::ostream&, std::istream&, MapsController&);
+  void removeRoad(std::ostream&, std::istream&, MapsController&);
+  void addOrder(std::ostream&, std::istream&, MapsController&);
+  void removeOrder(std::ostream&, std::istream&, MapsController&);
+  void optimizeCmd(std::ostream&, std::istream&, MapsController&);
+  void nextStep(std::ostream&, std::istream&, MapsController&);
+  void prevStep(std::ostream&, std::istream&, MapsController&);
+  void loadCmd(std::ostream&, std::istream&, MapsController&);
+  void newMapCmd(std::ostream&, std::istream&, MapsController&);
+  void switchMapCmd(std::ostream&, std::istream&, MapsController&);
+  void removeMapCmd(std::ostream&, std::istream&, MapsController&);
 
-  void listCities(std::ostream&, std::istream&, const Manager&);
-  void listTerminals(std::ostream&, std::istream&, const Manager&);
-  void listRoads(std::ostream&, std::istream&, const Manager&);
-  void listOrders(std::ostream&, std::istream&, const Manager&);
-  void listMaps(std::ostream&, std::istream&, const Manager&);
-  void showRoute(std::ostream&, std::istream&, const Manager&);
-  void saveCmd(std::ostream&, std::istream&, const Manager&);
+  void listCities(std::ostream&, std::istream&, const MapsController&);
+  void listTerminals(std::ostream&, std::istream&, const MapsController&);
+  void listRoads(std::ostream&, std::istream&, const MapsController&);
+  void listOrders(std::ostream&, std::istream&, const MapsController&);
+  void listMaps(std::ostream&, std::istream&, const MapsController&);
+  void showRoute(std::ostream&, std::istream&, const MapsController&);
+  void saveCmd(std::ostream&, std::istream&, const MapsController&);
 }
 
 #endif
